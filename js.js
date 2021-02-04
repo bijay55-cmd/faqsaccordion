79e942b88872412242f916a6a85d2d35bd82e171
bijay55-cmd <71406824+bijay55-cmd@users.noreply.github.com>
2021-02-04 11:42:53 +0000

    Update js.js

--- js.js
+++ js.js
@@ -1,29 +1,25 @@
-// Get all Accordion and Panel
-let accHeading = document.querySelectorAll(".accordion");
-let accPanel = document.querySelectorAll(".accordion-panel");
+// Set up the click event on the accordion itself
+// When a panel is clicked, that event will bubble up
+// to the accordion and can be handled there.
+document.querySelector(".accordion").addEventListener("click", function(event){
 
-for (let i = 0; i < accHeading.length; i++) {
-    // Execute whenever an accordion is clicked 
-    accHeading[i].onclick = function() {
-        if (this.nextElementSibling.style.maxHeight) {
-           hidePanels();     // Hide All open Panels 
-        } else {
-           showPanel(this);  // Show the panel
-        } 
-    };
-}
+  // Check to see if the clicked panel was the currently open one.
+  let alreadyActive = event.target.classList.contains("active");
+  
+  // Loop over all the panels an close each one
+  document.querySelectorAll(".panel").forEach(function(panel){
+    panel.style.maxHeight = "0";
+    panel.previousElementSibling.classList.remove("active");
+  });
 
-// Function to Show a Panel
-function showPanel(elem) {
-  hidePanels();
-  elem.classList.add("active");
-  elem.nextElementSibling.style.maxHeight = elem.nextElementSibling.scrollHeight + "px";
-}
-
-// Function to Hide all shown Panels
-function hidePanels() {
-  for (let i = 0; i < accPanel.length; i++) {
-      accPanel[i].style.maxHeight = null;
-      accHeading[i].classList.remove("active");
+  // If the clicked panel wasn't the already active one go ahead
+  // and open the clicked panel. Otherwise, do nothing and leave
+  // all the panels closed.
+  if(!alreadyActive){
+    // Then show the clicked panel which is accessible as event.target
+    event.target.classList.add("active");
+    var panel = event.target.nextElementSibling;
+    panel.style.maxHeight = panel.scrollHeight + "px";
   }
-}
+
+});
